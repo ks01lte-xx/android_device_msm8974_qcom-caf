66353797cda2ea60b7be1d7df11065e651fc00bb
1bdellatif <ptaoubi@gmail.com>
2023-10-14 03:06:17 +0100

    4

--- display/libgralloc/gpu.h
+++ display/libgralloc/gpu.h
@@ -24,7 +24,6 @@
 #include <string.h>
 
 #include <log/log.h>
-#include <cutils/ashmem.h>
 
 #include "gralloc_priv.h"
 #include "fb_priv.h"
--- display/libqservice/Android.mk
+++ display/libqservice/Android.mk
@@ -6,7 +6,7 @@ LOCAL_MODULE                  := libqservice
 LOCAL_MODULE_TAGS             := optional
 LOCAL_VENDOR_MODULE           := true
 LOCAL_SHARED_LIBRARIES        := $(common_libs) libbinder
-LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdqservice\" -Wno-sign-conversion
+LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdqservice\" -Wno-sign-conversion -DDO_NOT_CHECK_MANUAL_BINDER_INTERFACES
 LOCAL_CLANG                   := true
 LOCAL_HEADER_LIBRARIES        := display_headers generated_kernel_headers
 LOCAL_SRC_FILES               := QService.cpp \
